3deb03df3e6f6ed434031335a475af3290fb7389
Saruulbuyan Munkhtur <cedricmunkhtur2020@example.com>
2021-01-28 20:12:50 +0800

    initialization

---
+++ App/components/cardCarousel.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, FlatList} from 'react-native';
+
+const CardCarousel = () => (
+  <FlatList>
+    <View>
+      
+    </View>
+  </FlatList>
+);
+
+export default CardCarousel;
---
+++ App/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => (
+  <View>
+    <Text>Hello World</Text>
+  </View>
+);
+
+export default App;
